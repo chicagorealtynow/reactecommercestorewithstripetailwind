2335cdeea323ab59d461bc961f766a4d0ff28ade
chicagorealtynow <31782773+chicagorealtynow@users.noreply.github.com>
2025-01-28 14:13:24 -0500

    Add files via upload

--- store/src/productsStore.js
+++ store/src/productsStore.js
@@ -6,17 +6,20 @@ const productsArray = [
     {
         id: "price_1LnUTFDM1jwCEz8OGoOSXiSM",
         title: "Coffee",
-        price: 4.99
+        price: 4.99,
+        img: "https://m.media-amazon.com/images/I/71AkGIlaV7L._SY741_.jpg"
     },
     {
         id: "price_1LnUTxDM1jwCEz8OAqHYTwKQ",
         title: "Sunglasses",
-        price: 9.99
+        price: 9.99,
+        img: "https://m.media-amazon.com/images/I/51U+zB6bw1L._AC_SX679_.jpg"
     },
     {
         id: "price_1LnUUoDM1jwCEz8OvxIcJ7to",
         title: "Camera",
-        price: 39.99
+        price: 39.99,
+        img: "https://m.media-amazon.com/images/I/718tnaHeXyL._AC_CR0%2C0%2C0%2C0_SX480_SY360_.jpg"
     }
 ];
 
